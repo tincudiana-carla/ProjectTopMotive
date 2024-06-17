16044ce76eae762d83f44a6935ef7c50993e7cb5
tincudiana-carla <carla_diana@Yahoo.com>
2024-06-17 23:01:35 +0300

    Add project files.

---
+++ TM.DailyTrackR.ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TM.DailyTrackR.ViewModel
+{
+  using Prism.Mvvm;
+    using TM.DailyTrackR.Common;
+    using TM.DailyTrackR.Logic;
+
+    public sealed class MainWindowViewModel: BindableBase
+  {
+    public MainWindowViewModel()
+    {
+            LogicHelper.Instance.ExampleController.DeleteData();
+    }
+  }
+}
---
+++ TM.DailyTrackR/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace TM.DailyTrackR
+{
+  using System.Windows;
+  using TM.DailyTrackR.View;
+  using TM.DailyTrackR.ViewModel;
+
+  public partial class App : Application
+  {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      //Register View
+
+      MainWindow window = new();
+      window.DataContext = new MainWindowViewModel();
+      window.ShowDialog();
+    }
+  }
+}
